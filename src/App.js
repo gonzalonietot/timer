afcd2201eaf2b12a001bad9136cc2adeeada4dc5
gonzalonieto-soapboxx <gonzalo@soapboxx.us>
2023-01-26 13:10:22 -0300

    Timer with react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
+let timeHandler;
+let temp = 0;
+const start = (time, setTime) => {
+  timeHandler = setInterval(() => {
+    setTime(temp+=1);
+  }, 1000)
+  return time;
+};
+
+const clear = (setTime) => {
+  temp = 0;
+  setTime(0);
+  stop();
+};
+
+const stop = () => {
+  clearInterval(timeHandler)
+};
+
+const NewComponent = () => {
+  const [time, setTime] = useState(0);
+  return <div className="App-header">
+    <h1>Timer</h1>
+    <p>{time}</p>
+    <div style={{display: "flex"}}>
+      <button style={{marginRight: '10px'}} onClick={() => {start(time, setTime)}}>Start</button>
+      <button style={{marginRight: '10px'}} onClick={() => {stop(setTime)}}>Stop</button>
+      <button style={{marginRight: '10px'}} onClick={() => {clear(setTime)}}>Clear</button>
+    </div>
+  </div>
+};
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NewComponent />
     </div>
   );
 }
